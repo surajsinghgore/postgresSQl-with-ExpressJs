fd38913e3f19f920fdf00158936a7f320ce5f081
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-06-22 11:21:45 +0530

    create table

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const db =require('./Connection');
 app.get("/", async (req, res) => {
   try {
   
+    // create table
+    // await client.query(
+    //   "create table person(id serial primary key,name varchar(20) not null,age int)"
+    // );
 
 
     res.status(200).json({ success: 'connection successfully'});
