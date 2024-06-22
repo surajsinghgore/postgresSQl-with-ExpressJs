88c429f6948b4649f83d8dd4e817cb58da433bbe
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-06-22 11:24:54 +0530

    select data from table

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const app = express();
-const db =require('./Connection');
+const db = require("./Connection");
 
 app.get("/", async (req, res) => {
   try {
-  
     // create table
     // await client.query(
     //   "create table person(id serial primary key,name varchar(20) not null,age int)"
     // );
 
     // insert records
-    let username = "suraj";
-    let age = 21;
-    await db.pool.query(
-      `insert into person(name,age) values('${username}',${age})`
-    );
+    // let username = "suraj";
+    // let age = 21;
+    // await db.pool.query(
+    //   `insert into person(name,age) values('${username}',${age})`
+    // );
 
-    res.status(200).json({ success: 'connection successfully'});
+    // getting records
+
+    let data = await db.pool.query("select*from person");
+
+    res.status(200).json({ data: data.rows });
   } catch (error) {
     console.log(error);
   }
